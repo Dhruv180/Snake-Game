904f1a9073f925228f8255a103fa1d15b7a8cf09
Dhruv Mahajan <dhruvmahajan180@gmail.com>
2024-12-09 17:38:24 +0530

    Update Snake.c

--- Snake.c
+++ Snake.c
@@ -1,72 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <windows.h>
+#include <windows.h>  
 
 int i, j, height = 20, width = 20;
 int gameover, score, level;
-int x, y, fruitx, fruity;
+int x, y, fruitx, fruity, flag;
 int tailX[100], tailY[100];
-int nTail = 0;
-char direction; 
+int nTail = 0; // Initial snake length
 
-void setup() {
+void setup()
+{
     gameover = 0;
     x = height / 2;
     y = width / 2;
 
-    fruitx = rand() % (height - 2) + 1;
-    fruity = rand() % (width - 2) + 1;
+    // fruit initial position
+    do {
+        fruitx = rand() % 20;
+    } while (fruitx == 0);
+
+    do {
+        fruity = rand() % 20;
+    } while (fruity == 0);
 
     score = 0;
     level = 1;
-    nTail = 0;
-    direction = 'd'; // Default direction is right
+    nTail = 0; // Reset tail length
 }
 
-void draw() {
+void draw()
+{
     system("cls");
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
-            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
                 printf("#");
-            } else if (i == x && j == y) {
-                printf("O");
-            } else if (i == fruitx && j == fruity) {
-                printf("*");
-            } else {
-                int printed = 0;
+            else {
+                int isTail = 0;
                 for (int k = 0; k < nTail; k++) {
                     if (tailX[k] == i && tailY[k] == j) {
                         printf("o");
-                        printed = 1;
+                        isTail = 1;
                     }
                 }
-                if (!printed) {
-                    printf(" ");
+                if (!isTail) {
+                    if (i == x && j == y)
+                        printf("O");  // Snake head
+                    else if (i == fruitx && j == fruity)
+                        printf("*"); // Fruit
+                    else
+                        printf(" "); // Empty space
                 }
             }
         }
         printf("\n");
     }
-    printf("Score: %d | Level: %d\n", score, level);
-    printf("Press 'X' to exit.\n");
+
+    printf("Score = %d | Level = %d\n", score, level);
+    printf("Press X to quit the game\n");
 }
 
-void input() {
+void input()
+{
     if (_kbhit()) {
-        char ch = _getch();
-        switch (ch) {
-            case 'a': if (direction != 'd') direction = 'a'; break; // Left
-            case 's': if (direction != 'w') direction = 's'; break; // Down
-            case 'd': if (direction != 'a') direction = 'd'; break; // Right
-            case 'w': if (direction != 's') direction = 'w'; break; // Up
-            case 'x': gameover = 1; break;
+        switch (_getch()) {
+        case 'a':
+            if (flag != 2)  // Prevent moving in the opposite direction
+                flag = 1;
+            break;
+        case 's':
+            if (flag != 4)
+                flag = 2;
+            break;
+        case 'd':
+            if (flag != 1)
+                flag = 3;
+            break;
+        case 'w':
+            if (flag != 3)
+                flag = 4;
+            break;
+        case 'x':
+            gameover = 1;
+            break;
         }
     }
 }
 
-void logic() {
+void logic()
+{
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
@@ -82,52 +105,97 @@ void logic() {
         prevY = prev2Y;
     }
 
-    switch (direction) {
-        case 'a': y--; break; // Left
-        case 's': x++; break; // Down
-        case 'd': y++; break; // Right
-        case 'w': x--; break; // Up
+    switch (flag) {
+    case 1: // Move left
+        y--;
+        break;
+    case 2: // Move down
+        x++;
+        break;
+    case 3: // Move right
+        y++;
+        break;
+    case 4: // Move up
+        x--;
+        break;
+    default:
+        break;
     }
 
-    if (x <= 0 || x >= height - 1 || y <= 0 || y >= width - 1) {
+    // Check for game over conditions (wall collision or snake collision)
+    if (x < 0 || x > height - 1 || y < 0 || y > width - 1)
         gameover = 1;
-    }
 
     for (int i = 0; i < nTail; i++) {
-        if (tailX[i] == x && tailY[i] == y) {
+        if (tailX[i] == x && tailY[i] == y)
             gameover = 1;
-        }
     }
 
+    // Check if the snake eats the fruit
     if (x == fruitx && y == fruity) {
-        fruitx = rand() % (height - 2) + 1;
-        fruity = rand() % (width - 2) + 1;
+        while (1) {
+            fruitx = rand() % 20;
+            if (fruitx != 0)
+                break;
+        }
+
+        while (1) {
+            fruity = rand() % 20;
+            if (fruity != 0)
+                break;
+        }
+
         score += 10;
-        nTail++;
-        if (score % 50 == 0) level++;
+        nTail++; // Increase the snake's length
+
+        // Level-up after every 50 points
+        if (score % 50 == 0) {
+            level++;
+        }
     }
 }
 
 void restartOrQuit() {
-    printf("Game Over! Final Score: %d\n", score);
-    printf("Press 'R' to restart or 'X' to exit: ");
     char choice;
+    printf("Game Over! Final Score = %d\n", score);
+    printf("Press R to restart or X to quit: ");
     scanf(" %c", &choice);
-    if (choice == 'r' || choice == 'R') {
-        setup();
-    } else {
+    if (choice == 'R' || choice == 'r') {
+        setup(); 
+        while (!gameover) { 
+            draw();
+            input();
+            logic();
+
+           
+            int sleepTime = 100 - (level * 10);  
+            Sleep(sleepTime);  
+        }
+    } else if (choice == 'X' || choice == 'x') {
         exit(0);
     }
 }
 
-int main() {
+int main()
+{
     setup();
+
     while (!gameover) {
         draw();
         input();
         logic();
-        Sleep(200 - (level * 10)); 
+
+        int sleepTime;
+        if (level == 1) {
+            sleepTime = 200;  // Slower speed at Level 1
+        } else {
+            sleepTime = 100;  // Normal speed after Level 1
+        }
+
+        Sleep(sleepTime);  
     }
+
     restartOrQuit();
+
     return 0;
 }
